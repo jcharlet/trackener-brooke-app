3b2e9fea66547193e772cddc785336ff7de35a5b
Jeremie <charlet.jeremie@gmail.com>
2017-03-22 13:58:03 +0000

    add home screen design (header and footer)

--- index.android.js
+++ index.android.js
@@ -4,50 +4,173 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image
 } from 'react-native';
 
 export default class TrackenerBrookeApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.global}>
+                <View style={styles.header}>
+                    {/*<Image source={require('./images/pizza.jpg')} />*/}
+                    <View style={[styles.headerImageView]}>
+                        <View style={[styles.verticallyAligned]}>
+                            <Image
+                                source={require('./img/header-horse.png')}
+                                style={styles.headerImage}
+                            />
+                        </View>
+                    </View>
+                    <View style={styles.headerView}>
+                        <Text style={styles.headerText}>My Hackathon</Text>
+                    </View>
+                    <View style={styles.headerImageView}>
+                        <View style={[styles.verticallyAligned]}>
+                            <Image
+                                source={require('./img/header-settings.png')}
+                                style={styles.headerImage}
+                            />
+                        </View>
+                    </View>
+                </View>
+
+                <View style={styles.container}>
+                    <View>
+                        <Text>Start Ride</Text>
+                        <Text>Total Distance</Text>
+                    </View>
+
+                    <View>
+                        <Text>Stop</Text>
+                        <Text>Pause</Text>
+                        <Text>Location: </Text>
+                        <Text>Speed: </Text>
+                        <Text>Distance: </Text>
+                    </View>
+
+                </View>
+
+
+                <View style={[styles.footer]}>
+                    <View style={[styles.footerView, styles.tabSelected]}>
+                        <Image
+                            source={require('./img/tab-dashboard.png')}
+                            style={styles.footerImage}
+                        />
+                    </View>
+                    <View style={[styles.footerView]}>
+                        <Image
+                            source={require('./img/tab-exercising.png')}
+                            style={styles.footerImage}
+                        />
+                    </View>
+                    <View style={[styles.footerView]}>
+                        <Image
+                            source={require('./img/tab-calendar.png')}
+                            style={styles.footerImage}
+                        />
+                    </View>
+                </View>
+
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    global: {
+        flex: 1,
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        backgroundColor: 'white',
+        flexDirection: 'column',
+    },
+
+    header: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        backgroundColor: '#619b64',
+        flexBasis: 50,
+        flexGrow: 0
+    },
+    headerImageView: {
+        height: 50,
+        width: 50
+    },
+    verticallyAligned: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    headerImage: {
+        width: 32, height: 32, backgroundColor: '#619b64',
+    },
+
+    headerView: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    headerText: {
+        color: '#F5FCFF',
+        fontSize: 25,
+        flex: 1,
+        textAlignVertical: 'center'
+    },
+
+    container: {
+        flex: 1,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+
+
+    footer: {
+        flexBasis: 55,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        backgroundColor: '#619b64',
+    },
+    footerView: {
+        height: 47,
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    footerText: {
+        flex: 1,
+        color: '#F5FCFF',
+        textAlignVertical: 'center'
+    },
+    footerImage: {
+        width: 32, height: 32, backgroundColor: '#619b64',
+    },
+    tabSelected:{
+        height: 55,
+        borderStyle: 'solid',
+        borderColor: 'white',
+        // borderWidth: 1,
+        borderBottomWidth:8
+    },
+
+    border: {
+        borderStyle: 'solid',
+        borderColor: 'red',
+        borderWidth: 1
+    }
+
 });
 
 AppRegistry.registerComponent('TrackenerBrookeApp', () => TrackenerBrookeApp);
