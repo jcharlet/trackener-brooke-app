82c0f864a3e820f430c4a0c342ba3ab0d5798786
Jeremie <charlet.jeremie@gmail.com>
2017-05-02 10:23:50 +0100

    modif: minor add test dashboard url

--- src/config/config.js
+++ src/config/config.js
@@ -5,6 +5,7 @@ export const GPS_TIME_INTERVAL = 2000;
 export const GPS_DISTANCE_FILTER = 5;
 export const GPS_TIMEOUT_WATCH = 60000;
 
-export const TRACKENER_API="http://192.168.0.2:8000/api";
+export const TRACKENER_API="https://test.dashboard.trackener.com/api";
+// export const TRACKENER_API="http://192.168.0.2:8000/api";
 //export const GPS_MIN_ACCURACY = 21;
 export const GPS_MIN_ACCURACY = 50;
