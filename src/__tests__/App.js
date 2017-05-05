66f78b60ec98bc21fc00ee7dcd10ddb3b8472bec
Jeremie <charlet.jeremie@gmail.com>
2017-05-05 18:22:04 +0100

    add: testing with jest on appConfig repository

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,12 +1,13 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.android.js';
+module.exports = require('react-native-mock');
+import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   const tree = renderer.create(
-    <Index />
+    <App />
   );
 });
