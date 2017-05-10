5ef49cf3b41957ae2408458568ec28efdf444e9d
Jeremie <charlet.jeremie@gmail.com>
2017-05-10 18:21:43 +0100

    modif: enable generating bigger dataset (raise exception though if too much is stored in asyncStorage rides key)

---
+++ src/modules/storage/migration/__tests__/generateFakeData.test.js
@@ -0,0 +1,31 @@
+'use strict';
+const React = require('react');
+module.exports = require('react-native-mock');
+
+import * as migrateData from "../migrateData";
+import * as generateFakeData from "../generateFakeData";
+import {RIDES_COLL} from "../../localStorage/localRidesRepository";
+import * as userConfigRepository from "../../localStorage/userConfigRepository";
+import * as localRidesRepository from "../../localStorage/localRidesRepository";
+import * as localRidePositionsRepository from "../../localStorage/localRidePositionsRepository";
+import MockStorage from '../../localStorage/__mocks__/MockStorage';
+import FAKE_RIDES_JSON_FILE_PATH from '../../../../../resources/fakeSmall.json';
+const AsyncStorage = new MockStorage({});
+jest.setMock('AsyncStorage', AsyncStorage)
+
+
+beforeEach(() => {
+});
+
+describe('generateFakeData', () => {
+    it('generates data like expected', () => {
+
+        //given mock dataset
+        //when I generate data data
+        let rides = generateFakeData.generateRidesFromJsonFile(FAKE_RIDES_JSON_FILE_PATH, 2);
+
+        // then I should get as many more rides as there was initially
+        expect(rides.length).toBe(4)
+        expect.assertions(1);
+    })
+})
--- src/modules/storage/migration/generateFakeData.js
+++ src/modules/storage/migration/generateFakeData.js
@@ -3,10 +3,50 @@ import * as localRidesRepository from "../localStorage/localRidesRepository";
 import {
     AsyncStorage
 } from 'react-native';
-// import fakeRides from './fakeSmall.json';
-import fakeRides from '../../../../resources/fakeSmall5doc60minMinified.json';
+// import FAKE_RIDES_JSON_FILE_PATH from '../../../../resources/fakeSmall.json';
+import FAKE_RIDES_JSON_FILE_PATH from '../../../../resources/fakeSmall5doc60minMinified.json';
+import moment from "moment";
+
+const NB_OF_COPIES = 1;
+
+
 export const generateFakeData = () => {
-    console.log(fakeRides);
-    AsyncStorage.setItem('rides', JSON.stringify(fakeRides));
+    let rides = generateRidesFromJsonFile(FAKE_RIDES_JSON_FILE_PATH, NB_OF_COPIES);
+    AsyncStorage.setItem('rides', JSON.stringify(rides));
     AsyncStorage.setItem('totalDistance', '70829.0611293194');
+}
+
+export const generateRidesFromJsonFile= (rides, nbOfCopies) =>{
+    console.log("generate data ");
+    let newRides = [];
+    rides.map((ride)=>{
+
+        for (let i=0; i< nbOfCopies; i++){
+            //add 1 day to each ride + ride position
+            let date = moment(ride.date);
+            date.add(i, 'days');
+
+            let positions = ride.positions;
+            positions.map((position)=>{
+                let date = moment(position.timestamp);
+                date.add(i, 'days');
+                return {
+                    ...position,
+                    timestamp:date.valueOf()
+                }
+
+            })
+
+            newRides.push({
+                ...ride,
+                positions:positions,
+                date:date.format(),
+            });
+
+        }
+    })
+    newRides.sort(function(a,b) {return (moment(a.date).valueOf() > moment(b.date).valueOf()) ? 1 : -1;} );
+
+    return newRides;
+
 }
