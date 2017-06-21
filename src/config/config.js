cd6cd9c5d73f805109c73e9a97814776bd16ea66
Jeremie Charlet <jcharlet@Jeremies-MacBook-Pro.local>
2017-06-21 17:49:49 +0100

    release v1.4.1 with fix bug on iOS and config=prod

--- src/config/config.js
+++ src/config/config.js
@@ -5,8 +5,8 @@ export const GPS_TIME_INTERVAL = 2000;
 export const GPS_DISTANCE_FILTER = 5;
 export const GPS_TIMEOUT_WATCH = 60000;
 
-export const TRACKENER_API="https://test.dashboard.trackener.com/api";
-// export const TRACKENER_API="https://dashboard.trackener.com/api";
+// export const TRACKENER_API="https://test.dashboard.trackener.com/api";
+export const TRACKENER_API="https://dashboard.trackener.com/api";
 // export const TRACKENER_API="http://192.168.0.8:8000/api";
 //export const GPS_MIN_ACCURACY = 21;
 export const IS_TRACKENER_API_MOCKED=false;
