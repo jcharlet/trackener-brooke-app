be399364e268617e9bd92c0f677490ae6fb58fdc
Jeremie <charlet.jeremie@gmail.com>
2017-07-06 18:53:58 +0100

    upgrade android version, add location permission check on runtime in MainActivity on android

--- android/app/src/main/java/com/trackenerbrookeapp/MainActivity.java
+++ android/app/src/main/java/com/trackenerbrookeapp/MainActivity.java
@@ -1,6 +1,13 @@
 package com.trackenerbrookeapp;
+
 import com.bugsnag.BugsnagReactNative;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
 import com.facebook.react.ReactActivity;
 import com.joshblour.reactnativepermissions.ReactNativePermissionsPackage;
 
@@ -8,12 +15,22 @@ public class MainActivity extends ReactActivity {
 
     /*
     *   Enable BugSnag in android
+    *   Ask for location permission
     *
     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    BugsnagReactNative.start(this);
+        super.onCreate(savedInstanceState);
+        BugsnagReactNative.start(this);
+
+        //ask for location permission
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    0);
+        }
     }
 
     /**
