649e2e5d76d37c7b8cf9f0301a339a7cb4d11fb3
Jeremie <charlet.jeremie@gmail.com>
2017-07-07 12:08:05 +0100

    release: put back dev platform

--- src/config/configApi.js
+++ src/config/configApi.js
@@ -1,4 +1,4 @@
-// export const TRACKENER_API="https://test.dashboard.trackener.com/api";
-export const TRACKENER_API="https://dashboard.trackener.com/api";
+export const TRACKENER_API="https://test.dashboard.trackener.com/api";
+// export const TRACKENER_API="https://dashboard.trackener.com/api";
 // export const TRACKENER_API="http://192.168.0.8:8000/api";
 export const IS_TRACKENER_API_MOCKED=false;
